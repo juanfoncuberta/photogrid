a2ca1fdb4becaa9489a94901320eb587d8a2414e
Juan Foncuberta <juan.foncuberta@gmail.com>
2019-02-02 01:14:11 +0100

    Adding react-redux and the store to app

--- photogrid/src/App.js
+++ photogrid/src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import theme from './Styles/theme'
 import Template from './Layout/'
 import { ThemeProvider } from 'styled-components';
+import { Provider as StoreProvider} from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import store from './Store';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-           <Route path='/' component={Template}></Route>
-         </BrowserRouter>
-      </ThemeProvider> 
+      <StoreProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Route path='/' component={Template}></Route>
+          </BrowserRouter>
+        </ThemeProvider> 
+      </StoreProvider>
     );
   }
 }
